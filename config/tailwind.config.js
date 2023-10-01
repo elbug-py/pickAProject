dc839a0726784c053d58c18800a9924d775e5ee7
carmencitaap <cmavendano@miuandes.cl>
2023-10-01 01:27:04 -0300

    feat: more classes to safelist

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'custom-primary': '#F7AF3C'
+      }
     },
   },
   plugins: [
@@ -48,5 +51,57 @@ module.exports = {
       pattern:
         /^(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
     },
+    {
+      pattern:
+        /^(h-(?:0|1|2|3|4|5|6|7|8|9|10|11|12|14|16|20|24|28|32|36|40|44|48|52|56|60|64|72|80|96|auto|full))$/,
+    },
+    'h-1/2',
+    'h-1/3',
+    'h-2/3',
+    'h-1/4',
+    'h-2/4',
+    'h-3/4',
+    'h-1/5',
+    'h-2/5',
+    'h-3/5',
+    'h-4/5',
+    'h-1/6',
+    'h-2/6',
+    'h-3/6',
+    'h-4/6',
+    'h-5/6',
+    'h-screen',
+    {
+      pattern:
+        /^(w-(?:0|1|2|3|4|5|6|7|8|9|10|11|12|14|16|20|24|28|32|36|40|44|48|52|56|60|64|72|80|96|auto|full))$/,
+    },
+    'w-1/2',
+    'w-1/3',
+    'w-2/3',
+    'w-1/4',
+    'w-2/4',
+    'w-3/4',
+    'w-1/5',
+    'w-2/5',
+    'w-3/5',
+    'w-4/5',
+    'w-1/6',
+    'w-2/6',
+    'w-3/6',
+    'w-4/6',
+    'w-5/6',
+    'w-screen',
+    'w-1/12',
+    'w-2/12',
+    'w-3/12',
+    'w-4/12',
+    'w-5/12',
+    'w-6/12',
+    'w-7/12',
+    'w-8/12',
+    'w-9/12',
+    'w-10/12',
+    'w-11/12'
+
   ],
 }
