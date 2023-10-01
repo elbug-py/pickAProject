712b5d730a9ae5b541f118dfcc20878e1826662e
carmencitaap <cmavendano@miuandes.cl>
2023-09-30 22:44:15 -0300

    feat: devise's route and parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:password])
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   root "static#home"
+
+  devise_for :users
 end
