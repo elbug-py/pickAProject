e95c6e1ab7578a57002f81eb4b802ded4f76ea31
carmencitaap <cmavendano@miuandes.cl>
2023-10-01 16:54:54 -0300

    feat: Styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  enum role: {
+    admin: 0,
+    student: 1,
+    teacher: 2
+  }
 end
