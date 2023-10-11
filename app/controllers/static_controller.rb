7d74777d33429eeb7dcc990fb1488e7fd41cdde8
webtech <webtech@webtech-debian.webtech>
2023-10-10 23:21:57 -0300

    Vista profile basica

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,10 @@
 class StaticController < ApplicationController
     def home
     end
+
+    def profile
+        @user = current_user
+        @area = current_user.area
+        @projects = user.projects
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :projects
+
+  get 'profile', to: 'static#profile'
 end
