0b7482cdc91006b71b88603657ded252fae6f7b2
carmencitaap <cmavendano@miuandes.cl>
2023-10-31 14:39:11 -0300

    feat: binary changes to files due to migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_25_191821) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_31_170134) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,6 +70,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_25_191821) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "project_type", default: 1, null: false
+    t.date "start_date", default: "2023-10-31", null: false
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
